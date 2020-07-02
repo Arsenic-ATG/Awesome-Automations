64d14c4bc8ce5491a25fdfcc16c0d8e9b280e796
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-02 11:31:07 +0530

    add email regex

--- Phone-number finder/automation.py
+++ Phone-number finder/automation.py
@@ -11,6 +11,12 @@ phoneRegex = re.compile(r'''
  ''', re.VERBOSE)
  
  #create Email regex
+ emailRegex = re.compile(r'''(
+ [a-zA-Z0-9._%+-]+      #username
+ @                      # "@" symbol
+ [a-zA-Z0-9.-]+         #domain name
+ (\.[a-zA-Z]{2,4})      #dot something
+ )''', re.VERBOSE)
  
  #TODO : find matches to clipboard
  
