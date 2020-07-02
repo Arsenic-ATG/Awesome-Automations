46a6138da34f5eb3c90885340bf9c61f32e500a1
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-02 11:28:46 +0530

    add phone regex

--- Phone-number finder/automation.py
+++ Phone-number finder/automation.py
@@ -1,5 +1,17 @@
-import re
+import re,pyperclip
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') 
-mo = phoneNumRegex.search('My number is 415-555-4242.') 
-print('Phone number found: ' + mo.group())
+# create phone regex
+phoneRegex = re.compile(r'''
+( (\d{3}|\(\d{3}\))?                #Area code
+(\s|-|\.)?                          #Seperator
+(\d{3})                             #first 3 digits
+(\s|-|\.)                           #seperator
+(\d{4})                             #last 3 digits
+ (\s*(ext|x|ext.)\s*(\d{2,5}))? )   #extension
+ ''', re.VERBOSE)
+ 
+ #create Email regex
+ 
+ #TODO : find matches to clipboard
+ 
+ #TODO : copy matches to clipboard
