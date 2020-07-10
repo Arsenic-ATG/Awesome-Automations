bd6490e72c1d8e8814e8b83402494b389da1bd43
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-10 17:50:48 +0530

    create MyHandler class

--- file mover/file mover.py
+++ file mover/file mover.py
@@ -6,3 +6,9 @@ import os
 import json
 import time
 
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            os.rename(src, new_destination)
