fc158e1b53d592f09b777945abdb8ddaf7ccc528
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-10 17:51:55 +0530

    add main code without error handling

--- file mover/file mover.py
+++ file mover/file mover.py
@@ -12,3 +12,11 @@ class MyHandler(FileSystemEventHandler):
             src = folder_to_track + "/" + filename
             new_destination = folder_destination + "/" + filename
             os.rename(src, new_destination)
+
+folder_to_track = "/Users/ankursaini/Desktop/folder"
+folder_destination = "/Users/ankursaini/Desktop/newfolder"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+
+
