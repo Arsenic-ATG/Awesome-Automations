eb6d4b6ccdf33bccb010d1695bbb5e0a0dbd1c2a
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-10 17:49:47 +0530

    import necessary modules

---
+++ file mover/file mover.py
@@ -0,0 +1,8 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+# pip install watchdog for these packages to work
+
+import os
+import json
+import time
+
