98a0059f4429a6e83a62b00a8517151724156285
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-15 13:31:43 +0530

    write questions in quiz file

--- Generating Random Quiz Files/Project.py
+++ Generating Random Quiz Files/Project.py
@@ -30,6 +30,10 @@ for quizNum in range(number_of_quizes):
 		answerOptions = wrongAnswers + [correctAnswer]
 		random.shuffle(answerOptions)
 
-		# TODO: Write the question and answer options to the quiz file.
+		# Write the question and answer options to the quiz file.
+		quizFile.write("%s. What is the capital of %s?",questionNum+1,states[questionNum])
+		for i in range(4)
+			quizFile.write("%s. %s\n",'ABCD'[i],answerOptions[i])
+		quizFile.write("\n");
 		
 		# TODO: Write the answer key to a file.
