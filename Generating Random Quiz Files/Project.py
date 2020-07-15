5d1e1355e1e014845db2519f83d118677b0e3826
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-15 13:52:14 +0530

    create answer key

--- Generating Random Quiz Files/Project.py
+++ Generating Random Quiz Files/Project.py
@@ -4,7 +4,7 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
-number_of_quizes =3
+number_of_quizes =3 # can be manipulated according to the users requirement
 
 for quizNum in range(number_of_quizes):
 	# Create the quiz and answer key files.
@@ -31,9 +31,13 @@ for quizNum in range(number_of_quizes):
 		random.shuffle(answerOptions)
 
 		# Write the question and answer options to the quiz file.
-		quizFile.write("%s. What is the capital of %s?",questionNum+1,states[questionNum])
-		for i in range(4)
-			quizFile.write("%s. %s\n",'ABCD'[i],answerOptions[i])
+		quizFile.write("%s. What is the capital of %s?\n" % (questionNum + 1,
+            states[questionNum]))
+		for i in range(4):
+			quizFile.write("     %s. %s\n"%('ABCD'[i],answerOptions[i]))
 		quizFile.write("\n");
 		
-		# TODO: Write the answer key to a file.
+		# Write the answer key to a file.
+		answerKeyFile.write(" %s . %s\n"%(questionNum+1,'ABCD'[answerOptions.index(correctAnswer)]))
+	quizFile.close()
+	answerKeyFile.close()
