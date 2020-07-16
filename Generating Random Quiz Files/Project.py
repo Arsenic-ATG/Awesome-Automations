11592f72b5192eeaed866d830041e3a0d5dbae32
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-16 14:44:01 +0530

    created for indian states

--- Generating Random Quiz Files/Project.py
+++ Generating Random Quiz Files/Project.py
@@ -1,8 +1,36 @@
 import random
 
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+capitals = {
+	'Andhra Pradesh': 'Hyderabad',
+	'Arunachal Pradesh': 'Itanagar',
+	'Assam': 'Dispur',
+	'Bihar': 'Patna',
+	'Chhattisgarh': 'Raipur',
+	'Goa': 'Panaji',
+	'Gujarat': 'Gandhinagar',
+	'Haryana': 'Chandigarh',
+	'Himachal Pradesh': 'Shimla',
+	'Jammu and Kashmir': 'Srinagar/Jammu',
+	'Jharkhand': 'Ranchi',
+	'Karnataka': 'Bengaluru',
+	'Kerala': 'Thiruvananthapuram',
+	'Madhya Pradesh': 'Bhopal',
+	'Maharashtra': 'Mumbai',
+	'Manipur': 'Imphal',
+	'Meghalaya': 'Shillong',
+	'Mizoram': 'Aizawl',
+	'Nagaland': 'Kohima',
+	'Odisha': 'Bhubaneswar',
+	'Punjab': 'Chandigarh',
+	'Rajasthan': 'Jaipur',
+	'Sikkim': 'Gangtok',
+	'Tamil Nadu': 'Chennai',
+	'Telangana': 'Hyderabad',
+	'Tripura': 'Agartala',
+	'Uttar Pradesh': 'Lucknow',
+	'Uttarakhand': 'Dehradun',
+	'West Bengal': 'Kolkata'
+}
 
 number_of_quizes =3 # can be manipulated according to the users requirement
 
@@ -21,7 +49,7 @@ for quizNum in range(number_of_quizes):
 	random.shuffle(states)
 	
 	# Loop through all 50 states, making a question for each.
-	for questionNum in range(50):
+	for questionNum in range(29):
 		# Get right and wrong answers
 		correctAnswer = capitals[states[questionNum]]
 		wrongAnswers = list(capitals.values())
