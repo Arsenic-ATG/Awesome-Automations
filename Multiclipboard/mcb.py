7841f30fb635efedf83952b68bf8e1c625b431ff
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-23 09:16:46 +0530

    update skeleton script

--- Multiclipboard/mcb.py
+++ Multiclipboard/mcb.py
@@ -1,6 +1,7 @@
 # mcb.pyw - Saves and loads pieces of text to the clipboard.
 # Usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword.
-# py.exe mcb.pyw <keyword> - Loads keyword to clipboard. py.exe mcb.pyw list - Loads all keywords to clipboard.
+# 		 py.exe mcb.pyw <keyword> - Loads keyword to clipboard. 
+# 		 py.exe mcb.pyw list - Loads all keywords to clipboard.
 
 import shelve , pyperclip , sys
 
