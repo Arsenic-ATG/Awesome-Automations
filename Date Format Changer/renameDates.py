17a75ed8014d3b9f9c0317659497df6a75c9f4e7
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-30 12:18:26 +0530

    import required modules

---
+++ Date Format Changer/renameDates.py
@@ -0,0 +1,8 @@
+# Renameing the dates from Amarican style format (MM-DD-YYYY) to European style (DD-MM-YYYY)
+
+import shutil,os,re
+
+# Create regex for Amarican Style dates
+datePattern = re.compile(r"""
+	
+	""",re.VERBOSE)
