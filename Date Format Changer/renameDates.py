04244f52faebe8f46921938928f0efcd7b1b254f
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-30 17:52:27 +0530

    Create regex for Amarican Style dates

--- Date Format Changer/renameDates.py
+++ Date Format Changer/renameDates.py
@@ -3,6 +3,19 @@
 import shutil,os,re
 
 # Create regex for Amarican Style dates
-datePattern = re.compile(r"""
-	
+datePattern = re.compile(r"""^(.*?) # all text before the date
+	((0|1)?\d)- 					# one or two digits for the month
+	((0|1|2|3)?\d)- 				# one or two digits for the day
+	((19|20)\d\d) 					# four digits for the year
+	(.*?)$							# all text after the date
 	""",re.VERBOSE)
+
+# TODO: Loop over the files in the working directory. # TODO: Skip files without a date.
+
+# TODO: Get the different parts of the filename.
+
+# TODO: Form the European-style filename.
+
+# TODO: Get the full, absolute file paths.
+
+# TODO: Rename the files.
