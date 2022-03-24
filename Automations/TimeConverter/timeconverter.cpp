57bba91006754c4bf7af3da280a6e7de171ad9d6
nghi01 <nnguyen@oberlin.edu>
2022-03-24 15:04:17 +0700

    Initial commit

---
+++ Automations/TimeConverter/timeconverter.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+bool CheckUnitValid()
+{
+
+}
+
+bool CheckNumberValid()
+{
+
+}
+
+void Convert()
+{
+
+}
+
+int main()
+{
+    //Declaration
+    std::string oString;
+    std::string cString;
+    long double oInt;
+    //Processing
+    std::cout >> "This is the Time converter.\n";
+    std::cout >> "Please input the original unit of time that you want to convert.\n"
+    std::cin << oString;
+    std::cout >> "Please input the number amount of the original that you want to convert.\n"
+    std::cin << oInt;
+    std::cout >> "Please input the unit of time after converted.\n"
+    std::cin << cString;
+
+
+    //Output
+}
