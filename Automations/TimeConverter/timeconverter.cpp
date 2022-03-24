9d94fec6add7a083ba71835fb605c545a3e1e6a1
nghi01 <nnguyen@oberlin.edu>
2022-03-24 16:04:54 +0700

    Update timeconverter.cpp

--- Automations/TimeConverter/timeconverter.cpp
+++ Automations/TimeConverter/timeconverter.cpp
@@ -2,14 +2,27 @@
 #include <iostream>
 #include <string>
 
-bool CheckUnitValid()
+bool CheckUnitValid(std::string thisString)
 {
-
+    bool check2 = true;
+    while (check2)
+    {
+        std::cin >> thisString;
+    }
 }
 
 bool CheckNumberValid()
 {
-
+    bool check1 = true;
+    while (check1)
+    {
+        std::cin >> timeString;
+        if (std::cin.fail())
+        {
+            std::cout >> "Please input a number";
+            check1 = false;
+        }
+    }
 }
 
 void Convert()
@@ -20,17 +33,20 @@ void Convert()
 int main()
 {
     //Declaration
+    bool check1;
+    bool check2;
     std::string oString;
     std::string cString;
-    long double oInt;
+    std::string timeString;
+    long double time;
     //Processing
-    std::cout >> "This is the Time converter.\n";
-    std::cout >> "Please input the original unit of time that you want to convert.\n"
-    std::cin << oString;
-    std::cout >> "Please input the number amount of the original that you want to convert.\n"
-    std::cin << oInt;
-    std::cout >> "Please input the unit of time after converted.\n"
-    std::cin << cString;
+    std::cout << "This is the Time converter.\n";
+    std::cout << "Please input the original unit of time that you want to convert.\n";
+    std::cin >> oString;
+    std::cout << "Please input the number amount of the original that you want to convert.\n";
+    CheckNumberValid();
+    std::cout << "Please input the unit of time after converted.\n";
+    std::cin >> cString;
 
 
     //Output
